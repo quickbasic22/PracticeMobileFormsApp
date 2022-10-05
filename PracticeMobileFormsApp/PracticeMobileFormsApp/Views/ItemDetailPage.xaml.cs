54b7a8e0758e9f1227097940580a5ab02a1e6eb9
David Morrow <quickbasic44@outlook.com>
2022-10-05 15:41:40 -0400

    Add project files.

---
+++ PracticeMobileFormsApp/PracticeMobileFormsApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PracticeMobileFormsApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PracticeMobileFormsApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
